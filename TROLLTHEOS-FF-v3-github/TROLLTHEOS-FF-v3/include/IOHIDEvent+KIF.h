bfaae01d0528ae2af565212fe92e5ec8180723db
gira983 <138076434+gira983@users.noreply.github.com>
2026-02-25 11:00:32 +0300

    Delete TROLLTHEOS-FF-v3-github/TROLLTHEOS-FF-v3 directory

--- TROLLTHEOS-FF-v3-github/TROLLTHEOS-FF-v3/include/IOHIDEvent+KIF.h
+++
@@ -1,2 +0,0 @@
-typedef struct __IOHIDEvent * IOHIDEventRef;
-IOHIDEventRef kif_IOHIDEventWithTouches(NSArray *touches) CF_RETURNS_RETAINED;
--- TROLLTHEOS-FF-v3-github/TROLLTHEOS-FF-v3/platform_stub.c
+++
@@ -1,3 +0,0 @@
-int __isPlatformVersionAtLeast(unsigned int platform, unsigned int major, unsigned int minor, unsigned int patch) {
-    return 1;
-}
