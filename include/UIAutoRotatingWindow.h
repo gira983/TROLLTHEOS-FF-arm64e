768add38d767cf766760947cb4c5595c7e7c9279
gira983 <138076434+gira983@users.noreply.github.com>
2026-03-07 00:48:38 +0300

    Update UIAutoRotatingWindow.h

--- include/UIAutoRotatingWindow.h
+++ include/UIAutoRotatingWindow.h
@@ -11,6 +11,6 @@
 - (void)_didRemoveSubview:(UIView *)arg1;
 - (instancetype)_initWithFrame:(CGRect)arg1 attached:(BOOL)arg2;
 - (void)updateForOrientation:(UIInterfaceOrientation)arg1;
-- (instancetype)_initWithFrame:(CGRect)arg1 debugName:(NSString *)arg2 windowScene:(UIWindowScene *)arg3 API_AVAILABLE(ios(13.0));
+- (instancetype)_initWithFrame:(CGRect)arg1 debugName:(NSString *)arg2 windowScene:(UIWindowScene *)arg3;
 
 @end
